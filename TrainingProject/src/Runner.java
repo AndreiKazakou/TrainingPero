2a1fab0a87a40b3d0ebc77f92edb892088228961
Andrei_Kazakou <Andrei.Kazakou@gmail.com>
2014-01-29 19:29:21 +0300

    1

--- TrainingProject/src/Runner.java
+++ TrainingProject/src/Runner.java
@@ -11,10 +11,11 @@ public class Runner{
 	public static void main(String[] args) {
 		FileReader fileReader= new FileReader(args[0], args[1], args[2] , args[3]);
 		try {
-				
+				System.out.println("------------------------------------------------------");
 				fileReader.witeOutPutFile(fileReader.concatFiles(fileReader.readFiles()));
 				System.out.println("results of concatination:");
 				System.out.println(fileReader.concatFiles(fileReader.readFiles()));
+				System.out.println("------------------------------------------------------");
 				System.out.println("results of reg exp:");
 				System.out.println(fileReader.regXpFile(fileReader.concatFiles(fileReader.readFiles())));
 				
