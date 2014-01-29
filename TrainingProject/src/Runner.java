9bb11ed20a7907dacb02464dbae3848e4f969822
Andrei_Kazakou <Andrei.Kazakou@gmail.com>
2014-01-29 18:22:54 +0300

    new

--- TrainingProject/src/Runner.java
+++ TrainingProject/src/Runner.java
@@ -9,12 +9,11 @@ public class Runner{
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Please enter the path of three files in the strict order:");
-		System.out.println("1) Origing file;\n2) Concating file;\n3) Output file;\n");
-		
 		FileReader fileReader= new FileReader(args[0], args[1], args[2]);
 		try {
 				fileReader.witeOutPutFile(fileReader.concatFiles(fileReader.readFiles()));
+				System.out.println(fileReader.concatFiles(fileReader.readFiles()));
+				
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
